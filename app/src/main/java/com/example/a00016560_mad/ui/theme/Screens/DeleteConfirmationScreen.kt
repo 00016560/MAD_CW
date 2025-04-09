e62f52d6a612beee928f8b4d17c078ecaef6dfc9
00016560 <wiut00016560@gmail.com>
2025-04-09 14:09:49 +0500

    third commit

--- app/src/main/java/com/example/a00016560_mad/ui/theme/Screens/DeleteConfirmationScreen.kt
+++ app/src/main/java/com/example/a00016560_mad/ui/theme/Screens/DeleteConfirmationScreen.kt
@@ -1,20 +1,36 @@
 package com.example.a00016560_mad.ui.theme.Screens
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.example.a00016560_mad.R
 import com.example.a00016560_mad.ui.theme.ProductViewModel
 
 
+@SuppressLint("StateFlowValueCalledInComposition")
 @Composable
 fun DeleteProductScreen(navController: NavController, productId: Int, viewModel: ProductViewModel, paddingValues: PaddingValues) {
-    val product = viewModel.products.find { it.id == productId }
+    val product = viewModel.products.value.find { it.id == productId }
     product?.let {
         Column(
             modifier = Modifier.padding(paddingValues).fillMaxSize(),
@@ -22,7 +38,7 @@ fun DeleteProductScreen(navController: NavController, productId: Int, viewModel:
             verticalArrangement = Arrangement.Center
         ) {
             Text(
-                text = stringResource(R.string.product_delete_confirmation, it.name),
+                text = stringResource(R.string.product_delete_confirmation, it.brand),
                 fontSize = 16.sp,
                 fontWeight = FontWeight.Bold
             )
